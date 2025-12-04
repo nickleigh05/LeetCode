8103a98a22b1abd6623ef5052d8c223559ae806c
nickleigh05 <nickleigh05@icloud.com>
2025-12-04 05:38:03 +0000

    LeetCode 217
    Contains duplicate, NeetCode 1/150

--- NeetCode150/Arrays & Hashing/217.py
+++ NeetCode150/Arrays & Hashing/217.py
@@ -34,3 +34,12 @@ Constraints:
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        hashset = set()
+        
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
+
