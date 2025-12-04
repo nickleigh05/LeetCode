b8a8617660ec9f0233ce0ecaa8eeb41a0d75865c
nickleigh05 <nickleigh05@icloud.com>
2025-12-04 23:40:39 +0000

    LeetCode242
    Valid Anagram

--- NeetCode150/Arrays & Hashing/242.py
+++ NeetCode150/Arrays & Hashing/242.py
@@ -24,3 +24,14 @@ Follow up: What if the inputs contain Unicode characters? How would you adapt yo
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+            
+        smap = {}
+        tmap = {}
+        
+        for i in range(len(s)):
+            smap[s[i]] = 1 + smap.get(s[i], 0)
+            tmap[t[i]] = 1 + tmap.get(t[i], 0)
+        return smap == tmap
