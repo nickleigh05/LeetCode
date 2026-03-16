5fe1bb3bb7d14af74cb2c83330e6707a813bd63c
Nick Leigh <nickleigh05@icloud.com>
2026-03-16 15:17:05 -0700

    Problem number 8

---
+++ NeetCode150/36.py
@@ -0,0 +1,90 @@
+"""
+
+36. Valid Sudoku
+
+Medium
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+
+    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+    Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+
+    Input: board = 
+    [["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+    Output: true
+
+Example 2:
+
+    Input: board = 
+    [["8","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+    Output: false
+    Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Constraints:
+
+    board.length == 9
+    board[i].length == 9
+    board[i][j] is a digit 1-9 or '.'.
+
+"""
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        row_sets = [set() for _ in range(9)]
+        col_sets = [set() for _ in range(9)]
+        box_sets = [set() for _ in range(9)]
+
+        for row in range(9):
+            for col in range(9):
+                cell = board[row][col]
+
+                if cell == '.':
+                    continue
+
+                box = (row // 3) * 3 + (col // 3)
+
+                if cell in row_sets[row]:
+                    return False
+                row_sets[row].add(cell)
+
+                if cell in col_sets[col]:
+                    return False
+                col_sets[col].add(cell)
+
+                if cell in box_sets[box]:
+                    return False
+                box_sets[box].add(cell)
+
+        return True
+    
+
+
+
+
+
+    
