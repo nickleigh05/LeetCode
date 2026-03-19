0193a45b459b29f9ad0ef7ff8da3e3ecd7222cb6
Nicholas Leigh <nickleigh05@icloud.com>
2026-03-19 13:04:01 -0700

    Problem number 1748

---
+++ LeetCode/1748.py
@@ -0,0 +1,60 @@
+"""
+
+1748. Sum of Unique Elements
+
+Easy
+
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+Example 1:
+
+    Input: nums = [1,2,3,2]
+    Output: 4
+    Explanation: The unique elements are [1,3], and the sum is 4.
+
+Example 2:
+
+    Input: nums = [1,1,1,1,1]
+    Output: 0
+    Explanation: There are no unique elements, and the sum is 0.
+
+Example 3:
+
+    Input: nums = [1,2,3,4,5]
+    Output: 15
+    Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+Constraints:
+
+    1 <= nums.length <= 100
+    1 <= nums[i] <= 100
+
+"""
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        
+        freqdict = {}
+        sum = 0
+
+        for num in nums:
+            freqdict[num] = freqdict.get(num, 0) + 1
+
+        for key, value in freqdict.items():
+            if value == 1:
+                sum += key
+            else:
+                pass
+        return sum
+    
+
+
+
+
+
+
+
+
+
