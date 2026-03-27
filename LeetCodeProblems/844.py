b6ee82500e7a6203a9d589c9412a0f507ac3e6c7
Nicholas Leigh <nickleigh05@icloud.com>
2026-03-27 11:59:21 -0700

    Problem number 844

---
+++ LeetCodeProblems/844.py
@@ -0,0 +1,79 @@
+"""
+
+844. Backspace String Compare
+
+Easy
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+    Example 1:
+
+    Input: s = "ab#c", t = "ad#c"
+    Output: true
+    Explanation: Both s and t become "ac".
+
+Example 2:
+
+    Input: s = "ab##", t = "c#d#"
+    Output: true
+    Explanation: Both s and t become "".
+
+Example 3:
+
+    Input: s = "a#c", t = "b"
+    Output: false
+    Explanation: s becomes "c" while t becomes "b".
+
+Constraints:
+
+    1 <= s.length, t.length <= 200
+    s and t only contain lowercase letters and '#' characters.
+
+Follow up: Can you solve it in O(n) time and O(1) space?
+
+"""
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        
+        stack1 = []
+        stack2 = []
+
+        for char in s:
+            if char == '#':
+                if stack1:
+                    stack1.pop() 
+            else:
+                stack1.append(char)
+
+        for char in t:
+            if char == '#':
+                if stack2:
+                    stack2.pop()
+            else:
+                stack2.append(char)
+                
+        return stack1 == stack2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
