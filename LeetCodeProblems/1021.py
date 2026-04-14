53ff25cf0c5db8f3709f7612743d19a33f08860f
Nick Leigh <nickleigh05@icloud.com>
2026-04-14 13:55:50 -0700

    problem number 1021

---
+++ LeetCodeProblems/1021.py
@@ -0,0 +1,90 @@
+"""
+
+1021. Remove Outermost Parentheses
+
+Easy
+
+Hint
+
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+    For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+Example 1:
+
+    Input: s = "(()())(())"
+    Output: "()()()"
+    Explanation: 
+    The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+    After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+
+Example 2:
+
+    Input: s = "(()())(())(()(()))"
+    Output: "()()()()(())"
+    Explanation: 
+    The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+    After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+
+Example 3:
+
+    Input: s = "()()"
+    Output: ""
+    Explanation: 
+    The input string is "()()", with primitive decomposition "()" + "()".
+    After removing outer parentheses of each part, this is "" + "" = "".
+
+Constraints:
+
+    1 <= s.length <= 105
+    s[i] is either '(' or ')'.
+    s is a valid parentheses string.
+
+"""
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+
+        result = ""
+        depth = 0
+        
+        for char in s:
+            if char == '(':
+                if depth > 0:
+                    result += char
+                depth += 1
+            else: # char == ')'
+                depth -= 1
+                if depth > 0:
+                    result += char
+        
+        return result
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
