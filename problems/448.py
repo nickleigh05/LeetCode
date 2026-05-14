a853f6e140a2ebb02f64698f57436022c30f76fc
nickleigh <nickleigh05@icloud.com>
2026-05-13 22:10:40 -0700

    problem number 645

--- problems/448.py
+++ problems/448.py
@@ -47,3 +47,19 @@ class Solution:
 
 
 
+### Another solution that does not use extra space, but modifies the input array. ###
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+
+        for num in nums:
+            idx = abs(num) - 1
+            if nums[idx] > 0:
+                nums[idx] = -nums[idx]
+
+        result = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i + 1)
+
+        return result
