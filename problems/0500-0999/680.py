6003be621d7a621713f7c090122ecaaa00f7517c
Nicholas Leigh <nickleigh05@icloud.com>
2026-06-04 16:57:58 -0600

    Problem number 680

--- problems/0500-0999/680.py
+++ problems/0500-0999/680.py
@@ -32,3 +32,53 @@ Constraints:
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
+        def is_palindrome(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
+        
+        return True
+    
+
+
+
+
+
+
+
+
+
+
+
+### Brute Force ###
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        if s == s[::-1]:
+            return True
+
+        for i in range(len(s)):
+            temp = s[:i] + s[i+1:]
+            if temp == temp[::-1]:
+                return True           
+        return False
+    
+
+
+
+
+
+
