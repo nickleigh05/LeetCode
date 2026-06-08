54e96c5f27a3cf191a8dfcd50e9f8c45c073b137
nickleigh <nickleigh05@icloud.com>
2026-06-07 22:22:03 -0700

    Problem number 392

---
+++ problems/0001-0499/392.py
@@ -0,0 +1,84 @@
+"""
+
+392. Is Subsequence
+
+Easy
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+    Input: s = "abc", t = "ahbgdc"
+    Output: true
+
+Example 2:
+
+    Input: s = "axc", t = "ahbgdc"
+    Output: false
+
+Constraints:
+
+    0 <= s.length <= 100
+    0 <= t.length <= 104
+    s and t consist only of lowercase English letters.
+ 
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+ 
+"""
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        j = 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Follow up ###
+
+from bisect import bisect_left
+from collections import defaultdict
+
+def preprocess(t: str):
+    index = defaultdict(list)
+    for i, ch in enumerate(t):
+        index[ch].append(i)
+    return index
+
+def isSubsequence(s: str, index: dict) -> bool:
+    pos = 0
+
+    for ch in s:
+        if ch not in index:
+            return False
+        positions = index[ch]
+
+        i = bisect_left(positions, pos)
+        if i == len(positions):
+            return False
+        pos = positions[i] + 1
+
+    return True
+
+
+
+
