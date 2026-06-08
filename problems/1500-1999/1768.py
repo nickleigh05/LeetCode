7c1a6188c79a261557ad58ecb619883f0a4b2a12
nickleigh <nickleigh05@icloud.com>
2026-06-07 22:28:42 -0700

    Problem number 1768

--- problems/1500-1999/1768.py
+++ problems/1500-1999/1768.py
@@ -46,3 +46,69 @@ Constraints:
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         
+        result = []
+        i = 0
+        j = 0
+
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
+            i += 1
+            j += 1
+
+        result.append(word1[i:])
+        result.append(word2[j:])
+
+        return "".join(result)
+    
+
+
+
+
+
+
+
+
+
+
+
+### using zip ###
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+
+        result = []
+
+        for c1, c2 in zip(word1, word2):
+            result.append(c1)
+            result.append(c2)
+
+        result.append(word1[len(word2):])
+        result.append(word2[len(word1):])
+
+        return "".join(result)
+    
+
+
+
+
+
+
+
+
+
+### single index ###
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+        result = []
+        n = max(len(word1), len(word2))
+
+        for i in range(n):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+
+        return "".join(result)
