08fa81ca6c8fc02aa4f402db641ed349099b7049
nickleigh <nickleigh05@icloud.com>
2026-06-21 21:01:48 -0700

    Problem number 104

---
+++ problems/0001-0499/104.py
@@ -0,0 +1,85 @@
+"""
+
+104. Maximum Depth of Binary Tree
+
+Easy
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+        3
+       / \
+      9  20
+         / \
+        15  7
+
+    Input: root = [3,9,20,null,null,15,7]
+    Output: 3
+
+Example 2:
+
+    Input: root = [1,null,2]
+    Output: 2
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 104].
+    -100 <= Node.val <= 100
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+### iterative BFS solution ###
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        depth = 0
+        queue = deque([root])
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth
+    
+
+
+
+
+
+
+
+    
