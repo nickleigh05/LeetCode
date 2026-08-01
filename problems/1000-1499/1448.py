bc993457daf220949e4488db5b1d1e80c6f164c3
nickleigh <nickleigh05@icloud.com>
2026-08-01 15:47:27 -0700

    Prob 1448

---
+++ problems/1000-1499/1448.py
@@ -0,0 +1,89 @@
+"""
+
+1448. Count Good Nodes in Binary Tree
+
+Medium
+
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+
+Example 1:
+
+       ( 3 )
+      /     \
+     /       \
+  ( 1 )     ( 4 )
+  /         /     \
+ /         /       \
+( 3 )    ( 1 )    ( 5 )
+
+    Input: root = [3,1,4,3,null,1,5]
+    Output: 4
+    Explanation: Nodes in blue are good.
+    Root Node (3) is always a good node.
+    Node 4 -> (3,4) is the maximum value in the path starting from the root.
+    Node 5 -> (3,4,5) is the maximum value in the path
+    Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+
+       ( 3 )
+      /
+     /
+  ( 3 )
+  /     \
+ /       \
+( 4 )   ( 2 )
+
+    Input: root = [3,3,null,4,2]
+    Output: 3
+    Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+
+    Input: root = [1]
+    Output: 1
+    Explanation: Root is considered as good.
+
+Constraints:
+
+    The number of nodes in the binary tree is in the range [1, 10^5].
+    Each node's value is between [-10^4, 10^4].
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+                
+        def dfs(node, max_so_far):
+            if node is None:
+                return 0
+            count = 0
+            if node.val >= max_so_far:
+                count = 1
+            new_max = max_so_far
+            if node.val > new_max:
+                new_max = node.val
+            count += dfs(node.left, new_max)
+            count += dfs(node.right, new_max)
+            return count
+
+        return dfs(root, root.val)
+
+
+
+
+
+
+
+
+
+
+
