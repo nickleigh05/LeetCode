ce66bb90cf292f2d6d70388e927f10e55a38b950
Nicholas Leigh <nickleigh05@icloud.com>
2026-09-15 17:45:22 -0700

    Problem number 2591
    Less problems daily since tons of school work is taking most of my time

---
+++ problems/2500-2999/2591.py
@@ -0,0 +1,69 @@
+"""
+
+2591. Distribute Money to Maximum Children
+
+Easy
+
+You are given an integer money denoting the amount of money (in dollars) that you have and another integer children denoting the number of children that you must distribute the money to.
+
+You have to distribute the money according to the following rules:
+
+    All money must be distributed.
+    Everyone must receive at least 1 dollar.
+    Nobody receives 4 dollars.
+
+Return the maximum number of children who may receive exactly 8 dollars if you distribute the money according to the aforementioned rules. If there is no way to distribute the money, return -1.
+
+Example 1:
+
+    Input: money = 20, children = 3
+    Output: 1
+    Explanation: 
+    The maximum number of children with 8 dollars will be 1. One of the ways to distribute the money is:
+    - 8 dollars to the first child.
+    - 9 dollars to the second child. 
+    - 3 dollars to the third child.
+    It can be proven that no distribution exists such that number of children getting 8 dollars is greater than 1.
+
+Example 2:
+
+    Input: money = 16, children = 2
+    Output: 2
+    Explanation: Each child can be given 8 dollars.
+
+Constraints:
+
+    1 <= money <= 200
+    2 <= children <= 30
+
+"""
+
+class Solution:
+    def distMoney(self, money: int, children: int) -> int:
+
+        if money < children:
+            return -1
+        if money > 8 * children:
+            return children - 1
+        
+        k = (money - children) // 7
+        k = min(k, children)
+        rem = money - 8 * k - (children - k)
+        
+        if rem == 0:
+            if k == children:
+                return children - 1
+            return k
+        else:
+            if k == children - 1 and rem == 3:
+                return k - 1
+            return k
+
+
+
+
+
+
+
+
+
